17d17881de8e31a0629a2891fb8ce0c8cd5c5b0e
tangjie133 <1334875503@qq.com>
2024-09-29 10:48:34 +0800

    add

---
+++ TOF_SW/examples/tof/tof.ino
@@ -0,0 +1,26 @@
+#include "sensor.h"
+#include "tof_slave.h"
+#include <FreeRTOS.h>
+#include <task.h>
+#include <semphr.h>
+#include "Arduino.h"
+
+
+extern SemaphoreHandle_t xSemaphore;
+extern StaticSemaphore_t xMutexBuffer;
+
+
+
+void setup(){
+    Serial.begin(115200);
+    
+    i2cSlaveInit();//初始化I2C从机 
+    initI2CMaster();
+    
+}
+
+//主loop用于处理I2C从机问题
+void loop(){
+    i2cloop();//
+    delay(1);
+}
